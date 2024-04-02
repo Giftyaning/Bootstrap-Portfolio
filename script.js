610fefeaf1770094cefb4c555cd45e48852a14d5
Gifty <giftyaningg@gmail.com>
2024-04-02 21:13:15 +0100

    Hero

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 //Name typing 
- const name = "Gifty Aning";
- let index = 0;
- const typingInterval = setInterval(type, 250);
+const name = "Gifty Aning";
+const role = "Front-end Developer";
+let index = 0;
+let typingInterval;
 
- function type() {
-   document.getElementById("typing").innerText = name.slice(0, index);
-   index++;
+function type() {
+  if (index <= name.length) {
+    document.getElementById("typing").innerText = name.slice(0, index);
+  } else if (index <= name.length + 2) {
+    document.getElementById("typing").innerText = "";
+  } else {
+    document.getElementById("typing").innerText = role.slice(
+      0,
+      index - name.length - 3
+    );
+  }
+  index++;
 
-   if (index > name.length) {
-     clearInterval(typingInterval);
-   }
- }
+  if (index > name.length + role.length + 3) {
+    clearInterval(typingInterval);
+  }
+}
+
+typingInterval = setInterval(type, 100);
