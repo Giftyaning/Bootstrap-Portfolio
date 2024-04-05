e6b819bf25ecb409b2869b85afa917cb55c54675
Gifty <giftyaningg@gmail.com>
2024-04-05 18:01:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -23,3 +23,41 @@ function type() {
 }
 
 typingInterval = setInterval(type, 100);
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  var skills = document.querySelectorAll(".skill");
+
+  skills.forEach(function (skill) {
+    skill.addEventListener("mouseenter", function () {
+      var skillId = skill.getAttribute("id");
+      var skillName = getSkillName(skillId);
+      if (skillName) {
+        skill.innerHTML = "<span>" + skillName + "</span>";
+      }
+    });
+
+    skill.addEventListener("mouseleave", function () {
+      var skillId = skill.getAttribute("id");
+      var skillIconClass = "bx bxl-" + skillId;
+      skill.innerHTML = "<i class='" + skillIconClass + "'></i>";
+    });
+  });
+
+  // Function to get the skill name based on the ID
+  function getSkillName(skillId) {
+    // Add your custom logic here to map skill IDs to their names
+    switch (skillId) {
+      case "html":
+        return "HTML";
+      case "css":
+        return "CSS";
+      case "javascript":
+        return "JavaScript";
+      case "react":
+        return "React.js";
+      default:
+        return null;
+    }
+  }
+});
