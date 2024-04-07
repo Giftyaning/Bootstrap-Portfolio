84cddfa0ea392dadc2ed254c1dda228fa46543cf
Gifty <giftyaningg@gmail.com>
2024-04-07 23:26:36 +0100

    Animations

--- script.js
+++ script.js
@@ -68,3 +68,48 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 });
+
+//Fade-in animations
+ window.addEventListener("scroll", function () {
+   const fadeIns = document.querySelectorAll(".fade-in");
+
+   fadeIns.forEach((fadeIn) => {
+     if (isElementInViewport(fadeIn)) {
+       fadeIn.classList.add("active");
+     }
+   });
+ });
+
+ function isElementInViewport(el) {
+   const rect = el.getBoundingClientRect();
+   return (
+     rect.top >= 0 &&
+     rect.left >= 0 &&
+     rect.bottom <=
+       (window.innerHeight || document.documentElement.clientHeight) &&
+     rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+   );
+ }
+
+ //Slide-in right animation
+window.addEventListener("scroll", function () {
+  const slideIns = document.querySelectorAll(".slide-in-right");
+
+  slideIns.forEach((slideIn) => {
+    if (isElementInViewport(slideIn)) {
+      slideIn.classList.add("active");
+    }
+  });
+});
+
+function isElementInViewport(el) {
+  const rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
